c14c524da7cc991bbf5ed8423df41c82c6d8995b
Glebysheck <sedringleb2@gmail.com>
2023-07-11 05:39:52 +0300

    fix delete sort

--- app/Http/Controllers/SortController.php
+++ app/Http/Controllers/SortController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SortResource;
+use App\Models\Consumable;
+use App\Models\Detail;
+use App\Models\ListConsumables;
+use App\Models\ListDetails;
 use App\Models\Part;
 use App\Models\Sort;
 use App\Models\TypePart;
@@ -42,10 +46,20 @@ class SortController extends Controller
     public function delete(Request $request)
     {
         Sort::destroy($request->all()['id']);
-        TypePart::where('sort_id', $request->all()['id'])->delete();
         foreach (TypePart::where('sort_id', $request->all()['id'])->get()->toArray() as $type_part)
         {
             Part::where('type_parts_id', $type_part['id'])->delete();
         }
+        TypePart::where('sort_id', $request->all()['id'])->delete();
+        foreach (Detail::where('sort_id', $request->all()['id'])->get()->toArray() as $detail)
+        {
+            ListDetails::where('detail_id', $detail['id'])->delete();
+        }
+        Detail::where('sort_id', $request->all()['id'])->delete();
+        foreach (Consumable::where('sort_id', $request->all()['id'])->get()->toArray() as $detail)
+        {
+            ListConsumables::where('consumables_id', $detail['id'])->delete();
+        }
+        Consumable::where('sort_id', $request->all()['id'])->delete();
     }
 }
